253118cf5e6c3beec72659d8d23bdf3b48e3bb60
Shovon Nag <Shovanr06@gmail.com>
2023-08-02 20:21:42 +0600

    Email Front End Complete

--- resources/views/components/customer/email-form.blade.php
+++ resources/views/components/customer/email-form.blade.php
@@ -5,14 +5,36 @@
                 <div class="card-body">
                     <h4>EMAIL CAMAIGN</h4>
                     <br/>
-                    <label>Your email address</label>
+                    <label>Subject</label>
                     <input id="subject" placeholder="Subject" class="form-control" type="text"/>
-                    <label>Your email address</label>
+                    <label>Content</label>
                     <textarea id="content" name="content"  placeholder="content" class="form-control" type="text"></textarea>
                     <br/>
-                    <button onclick="VerifyEmail()"  class="btn w-100 float-end btn-primary">Next</button>
+                    <button onclick="SendEmail()"  class="btn w-100 float-end btn-primary">Next</button>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+
+<script>
+
+    async function SendEmail(){
+
+        let subject = document.getElementById("subject").value;
+        let content = document.getElementById("content").value;
+
+        showLoader();
+        let res = await axios.post("/emailCampaign",{subject:subject,content:content});
+        hideLoader();
+
+        if(res.status===200 && res.data['status']==='success'){
+            successToast(res.data['message']);
+        }else{
+            errorToast(res.data['message']);
+        }
+    }
+
+
+</script>
